5a498a12b2907490ca57c86b34c37605c84f1946
Patrick Cloutier <pcloutier@connexe.ca>
2016-11-27 15:25:19 -0500

    Initial commit

---
+++ Src/Kakasi.NET.Interop/KakasiLib.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Kakasi.NET.Interop
+{
+    /// <summary>
+    /// Kakasi library wrapper
+    /// </summary>
+    public static class KakasiLib
+    {
+
+        #region Externs
+
+        /// <summary>
+        /// Set DLL search directory
+        /// </summary>
+        /// <param name="lpPathName"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string lpPathName);
+
+        /// <summary>
+        /// Get procedure address
+        /// </summary>
+        /// <param name="hModule"></param>
+        /// <param name="procName"></param>
+        /// <returns></returns>
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        /// <summary>
+        /// Load library
+        /// </summary>
+        /// <param name="lpFileName"></param>
+        /// <returns></returns>
+        [DllImport("kernel32", SetLastError = true)]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        /// <summary>
+        /// Free library
+        /// </summary>
+        /// <param name="hModule"></param>
+        /// <returns></returns>
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FreeLibrary(IntPtr hModule);
+
+        #endregion
+
+        #region Delegates
+
+        /// <summary>
+        /// Kakasi get options arguments delegate
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int KakasiGetoptArgv([In] int size, [In] string[] param);
+
+        /// <summary>
+        /// Kakasi main execution method delegate
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate IntPtr KakasiDo(byte[] str);
+
+        /// <summary>
+        /// Kakasi get options arguments
+        /// </summary>
+        private static KakasiGetoptArgv _kakasiGetoptArgv;
+
+        /// <summary>
+        /// Kakasi main execution method
+        /// </summary>
+        private static KakasiDo _kakasiDo;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Kakasi library instance pointer
+        /// </summary>
+        private static IntPtr KakasiLibPtr;
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Init Kakasi library
+        /// </summary>
+        public static void Init()
+        {
+
+            // Lib path
+            var kakasiLibPath = Path.Combine(Environment.CurrentDirectory,
+                Environment.Is64BitProcess ? @"x64\" : @"x86\");
+
+            // Set search path
+            SetDllDirectory(kakasiLibPath);
+
+            // Load library
+            KakasiLibPtr = LoadLibrary(Path.Combine(kakasiLibPath, @"libkakasi.dll"));
+
+            // Check for errors
+            if (KakasiLibPtr != null)
+            {
+
+                // Loaded correctly
+                _kakasiGetoptArgv =
+                    (KakasiGetoptArgv)
+                    Marshal.GetDelegateForFunctionPointer(GetProcAddress(KakasiLibPtr, "kakasi_getopt_argv"),
+                        typeof(KakasiGetoptArgv));
+
+                _kakasiDo =
+                    (KakasiDo)
+                    Marshal.GetDelegateForFunctionPointer(GetProcAddress(KakasiLibPtr, "kakasi_do"), typeof(KakasiDo));
+
+            }
+            else
+            {
+
+                // Get last Win32 error                
+                var win32Error = Marshal.GetLastWin32Error();
+
+                // Check if defined
+                if (win32Error != 0)
+                {
+
+                    // Throw it
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+                // Unknown error
+                throw new Exception("Unable to load Kakasi library");
+
+            }
+
+        }
+
+        /// <summary>
+        /// Dispose of the library instance
+        /// </summary>
+        public static void Dispose()
+        {
+            if (KakasiLibPtr != IntPtr.Zero)
+            {
+                FreeLibrary(KakasiLibPtr);
+            }
+        }
+
+        /// <summary>
+        /// Set Kakasi library params
+        /// </summary>
+        /// <param name="params"></param>
+        public static void SetParams(string[] @params)
+        {
+
+            _kakasiGetoptArgv.Invoke(@params.Length, @params);
+
+        }
+
+        /// <summary>
+        /// Execute Kakasi action
+        /// </summary>
+        /// <param name="japanese"></param>
+        /// <returns></returns>
+        public static string DoKakasi(string japanese)
+        {
+
+            // Get EUC-JP encoding
+            var encoding = Encoding.GetEncoding("euc-jp");
+
+            // Get bytes
+            var japaneseBytes = encoding.GetBytes(japanese);
+            var callBytes = new byte[japaneseBytes.Length + 1];
+            Buffer.BlockCopy(japaneseBytes, 0, callBytes, 0, japaneseBytes.Length);
+
+            // Get result pointer
+            var resultPtr = _kakasiDo.Invoke(callBytes);
+
+            // Extract result bytes
+            var resultBytes = new List<byte>();
+            var currentByteIndex = 0;
+            for (;;)
+            {
+                var currentByte = Marshal.ReadByte(resultPtr, currentByteIndex);
+                if (currentByte != 0)
+                {
+                    resultBytes.Add(currentByte);
+                    currentByteIndex++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Get result string
+            var decodedResult = encoding.GetString(resultBytes.ToArray());
+
+            // Return it
+            return decodedResult;
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Src/Kakasi.NET.TestApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Kakasi.NET.Interop;
+
+namespace Kakasi.NET.TestApp
+{
+
+    /// <summary>
+    /// Test program
+    /// </summary>
+    class Program
+    {
+
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            // Initialize library
+            KakasiLib.Init();
+
+            // Set params to get Furigana
+            // NOTE: Use EUC-JP encoding as the wrapper will encode/decode using it
+            KakasiLib.SetParams(new[] { "kakasi", "-ieuc", "-f", "-JH", "-w" });
+
+            // Get furigana from this book: アンブラとパン先生 by 伊藤 綾子
+            // http://itunes.apple.com/us/book/id1144287620
+
+            // Set source sentence
+            var value1 = @"この森は昼でも薄暗く、気味が悪いので、村人は誰も近づかないのでした。";
+
+            // Get furigana
+            var resultValue1 = KakasiLib.DoKakasi(value1);
+
+            // Expected result
+            // この 森[もり] は 昼[ひる] でも 薄暗く[うすぐらく] 、 気味[きみ] が 悪い[わるい] ので 、 村人[むらびと] は 誰も[だれも] 近づ[ちかづ] かないのでした 。
+            Debug.Assert(resultValue1 == @"この 森[もり] は 昼[ひる] でも 薄暗く[うすぐらく] 、 気味[きみ] が 悪い[わるい] ので 、 村人[むらびと] は 誰も[だれも] 近づ[ちかづ] かないのでした 。");
+
+            // Get furigana from this book: プカプカ島の謎 by 伊藤 綾子
+            // http://itunes.apple.com/us/book/id1146306209
+
+            // Set source sentence
+            var value2 = @"この島の住人を紹介しましょう";
+
+            // Get furigana
+            var resultValue2 = KakasiLib.DoKakasi(value2);
+
+            // Expected result
+            // この 島[しま] の 住人[じゅうにん] を 紹介[しょうかい] しましょう
+            Debug.Assert(resultValue2 == @"この 島[しま] の 住人[じゅうにん] を 紹介[しょうかい] しましょう");
+
+            // Dispose of library
+            KakasiLib.Dispose();            
+
+        }
+
+    }
+}
